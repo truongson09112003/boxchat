fc850937ade43d081bb00bc39027fc54275d7b30
FIRST_NAME LAST_NAME <truongsonpt.80@gmail.com>
2022-07-07 15:35:49 +0700

    Update

--- src/components/Firebase/Config.js
+++ src/components/Firebase/Config.js
@@ -20,10 +20,10 @@ firebase.analytics();
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-if (window.location.hostname === 'localhost') {
-    auth.useEmulator('http://localhost:9099');
-    db.useEmulator('localhost', '8080');
-}
+// if (window.location.hostname === 'localhost') {
+//     auth.useEmulator('http://localhost:9099');
+//     db.useEmulator('localhost', '8080');
+// }
 
 export { db, auth };
 export default firebase;
